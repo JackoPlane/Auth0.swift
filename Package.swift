de91b2df51271337bb7501644ce16d119644b5e9
Jack Perry <JackoPlane@users.noreply.github.com>
2021-01-18 17:18:15 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     products: [.library(name: "Auth0", type: .dynamic, targets: ["Auth0"])],
     dependencies: [
         .package(url: "https://github.com/auth0/SimpleKeychain.git", .upToNextMajor(from: "0.12.0")),
-        .package(url: "https://github.com/JackoPlane/JWTDecode.swift.git", .upToNextMajor(from: "2.5.0")),
+        .package(url: "https://github.com/JackoPlane/JWTDecode.swift.git", .branch("master")),
         .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", .upToNextMajor(from: "9.0.0"))
